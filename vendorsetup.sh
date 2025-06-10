1808eb5b9b8e3d4ce21e4ff66f18a3cc919fde67
HELLINFIX <mridulsharma57570@gmail.com>
2025-06-10 09:34:59 +0000

    spaced: update vendorsetup url

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,10 +6,10 @@ KT=kernel/realme/mt6781/build.sh
 if ! [ -a $KT ]; then git clone https://github.com/realme-mt6781-dev/android_kernel_realme_mt6781.git kernel/realme/mt6781
 fi
 MTK_SEPOLICY=device/mediatek/sepolicy_vndr/SEPolicy.mk
-if ! [ -a $MTK_SEPOLICY ]; then git clone https://github.com/realme-mt6781-dev/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+if ! [ -a $MTK_SEPOLICY ]; then git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 fi
 MTK=hardware/mediatek/Android.bp
-if ! [ -a $MTK ]; then git clone https://github.com/realme-mt6781-dev/android_hardware_mediatek hardware/mediatek
+if ! [ -a $MTK ]; then git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek
 fi
 OPLUS=hardware/oplus/Android.bp
 if ! [ -a $OPLUS ]; then git clone https://github.com/LineageOS/android_hardware_oplus hardware/oplus
