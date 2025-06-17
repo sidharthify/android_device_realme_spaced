47b93e311fb21782fdcdf0900350b36b8808de80
sidharthify <wednisegit@gmail.com>
2025-06-17 15:13:38 +0000

    spaced: Build common Light HAL
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- device.mk
+++ device.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # Kernel
 TARGET_KERNEL_DIR := $(LOCAL_PATH)-kernel
 TARGET_PREBUILT_KERNEL := $(TARGET_KERNEL_DIR)/Image.gz
